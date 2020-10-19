4632f06cf22593b88eda763366c1d93b487001ed
DragonTamrr <parker.douglas.500@k12.friscoisd.org>
2020-10-19 10:11:05 -0500

    Added a losing screen

--- WhackAMole/ViewController.swift
+++ WhackAMole/ViewController.swift
@@ -12,8 +12,11 @@ class ViewController: UIViewController {
     
     let buttonToPress = UIButton()
     let scoreLabel = UILabel.init()
+    let lostLabel = UILabel.init()
+    let highScoreLabel = UILabel.init()
     var screenSize = UIScreen.main.bounds
     var score = 0
+    var highScore = 0
     var repeatTimer = true
     var firstPress = true
     var timer = Timer.init()
@@ -26,18 +29,26 @@ class ViewController: UIViewController {
         
         
         scoreLabel.frame = CGRect(x: 10, y: 15, width: 100, height: 50)
+        lostLabel.frame = CGRect(x: screenSize.width / 2 - 95, y: screenSize.height / 2 - 100, width: 300, height: 50)
+        highScoreLabel.frame = CGRect(x: screenSize.width / 2 - 50, y: 15, width: 200, height: 50)
         buttonToPress.frame = CGRect(x: screenSize.width / 2 - 50, y: screenSize.height / 2 - 25, width: 100, height: 50)
         
         screenSize = UIScreen.main.bounds
+        highScoreLabel.attributedText = NSAttributedString(string: "High Score: \(highScore)")
+        lostLabel.attributedText = NSAttributedString(string: "")
         scoreLabel.attributedText = NSAttributedString(string: "")
         
+        lostLabel.textColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
+        
         buttonToPress.setTitle("Tap to Start", for: .normal)
-        buttonToPress.backgroundColor = UIColor(red: 0.1, green: 1.0, blue: 0.0, alpha: 1)
+        buttonToPress.backgroundColor = UIColor(red: 0.37, green: 0.85, blue: 0.21, alpha: 1)
         buttonToPress.layer.cornerRadius = 10.0
         buttonToPress.addTarget(self, action: #selector(buttonAction(_:)), for: .touchDown)
         
         view.addSubview(buttonToPress)
         view.addSubview(scoreLabel)
+        view.addSubview(lostLabel)
+        view.addSubview(highScoreLabel)
         self.view = view
         // Do any additional setup after loading the view.
         
@@ -59,16 +70,23 @@ class ViewController: UIViewController {
     
     @objc func buttonAction(_ sender:UIButton!) {
         print("Button tapped")
+        lostLabel.attributedText = NSAttributedString(string: "")
         timer.invalidate()
         buttonToPress.setTitle("Tap me", for: .normal)
         buttonToPress.frame = CGRect(x: Int.random(in : 100...Int(screenSize.width - 100)), y: Int.random(in : 50...Int(screenSize.height - 100)), width: 100, height: 50)
         
         if !firstPress {
             score += 1
+            if score > highScore {
+                highScore = score
+                highScoreLabel.attributedText = NSAttributedString(string: "High Score: \(highScore)")
+            }
         }
         else {
             firstPress = false
         }
+        
+        
         
         scoreLabel.attributedText = NSAttributedString(string: "Score: \(score)")
         
@@ -83,6 +101,17 @@ class ViewController: UIViewController {
         buttonToPress.frame = CGRect(x: Int.random(in : 100...Int(screenSize.width - 100)), y: Int.random(in : 50...Int(screenSize.height - 50)), width: 100, height: 50)
         score -= 1
         scoreLabel.attributedText = NSAttributedString(string: "Score: \(score)")
+        
+        if score < 0{
+            firstPress = true
+            score = 0
+            buttonToPress.frame = CGRect(x: screenSize.width / 2 - 50, y: screenSize.height / 2 - 25, width: 100, height: 50)
+            lostLabel.attributedText = NSAttributedString(string: "You Lost! Tap to try again!")
+            scoreLabel.attributedText = NSAttributedString(string: "")
+            buttonToPress.setTitle("Tap to Start", for: .normal)
+            timer.invalidate()
+            
+        }
     }
 }
 
